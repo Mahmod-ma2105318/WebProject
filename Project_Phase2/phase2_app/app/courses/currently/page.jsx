97b691abe8ed5305a41e1d547f5aaa7bb0940f63
Mahmod-ma2105318 <ma2105318@qu.edu.qa>
2025-05-10 02:02:02 +0300

    Reapply "Merge branch 'main' of https://github.com/Mahmod-ma2105318/WebProject"
    This reverts commit c646130018e4241ce0ba0681e8ffcf25cd7f8b24.

---
+++ Project_Phase2/phase2_app/app/courses/currently/page.jsx
@@ -0,0 +1,23 @@
+import repo from '@/app/repo/repo';
+import React from 'react';
+//This page will be added in the student part, especially in the currentlyTakenCourse
+//دي الي بقولك حاول فيها لاين 17
+
+export default async function page() {
+  const currentlyTakenCourses = await repo.showCurrentCourses({ studentId: 1 });
+
+  return (
+    <>
+      {currentlyTakenCourses.map((register, index) => (
+        <div key={index} className="course-card">
+          <div className="course-name">{register.section.course.name}</div>
+          <div className="course-category">{register.section.course.category}</div>
+          <div className="course-credits">Credits: {register.section.course.credits}</div>
+          <div className="course-instructor">
+            Instructor: {register.section.instructor?.user?.username || 'N/A'}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
